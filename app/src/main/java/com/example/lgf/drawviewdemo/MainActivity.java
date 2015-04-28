7d18aae79e92f5cf360c2fa7c7e52c5957b16bed
aaa <813701754@qq.com>
2015-04-28 11:12:29 +0800

    7777777

--- app/src/main/java/com/example/lgf/drawviewdemo/MainActivity.java
+++ app/src/main/java/com/example/lgf/drawviewdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity  extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
