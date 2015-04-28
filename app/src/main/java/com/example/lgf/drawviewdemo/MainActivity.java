8c5d36d41041fbb02250047c6ade1966fe839d78
aaa <813701754@qq.com>
2015-04-28 10:56:01 +0800

    sddsdsdsdsddddsdss

--- app/src/main/java/com/example/lgf/drawviewdemo/MainActivity.java
+++ app/src/main/java/com/example/lgf/drawviewdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.lgf.drawviewdemo;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity {
+public class MainActivity  extends ActionBarActivity{
 //skjsds
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
